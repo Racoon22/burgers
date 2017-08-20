882cb74c398e9843c77a6ce5cd6a8e2fab7942f8
Alexandr Antonov <antonovsasha22@mail.ru>
2017-08-20 21:34:46 +0800

    Map added

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
 $(document).ready(function () {
 
+  var md = new MobileDetect(window.navigator.userAgent),
+    isMobile = md.mobile();
+
+    var scrollToSection = function (direction) {
+        var section = defineSection(sections);
+
+        if (direction == 'up' && section.nextSection.length) { // скроллим вверх
+            performTransition(section.nextSection.index());
+        }
+
+        if (direction == 'down' && section.prevSection.length) { //скроллим вниз
+            performTransition(section.prevSection.index());
+        }
+    }
+
     $('.header__menu-link').on('click', function (e) {
         e.preventDefault()
         var fullScreenMenu = $('#fullscreen-menu');
@@ -68,7 +83,8 @@ $(document).ready(function () {
     }
 
 
-    $('.wrapper').on('wheel', function (e) {
+    $('.wrapper').on({
+        wheel: function (e) {
         var deltaY = e.originalEvent.deltaY,
             section = defineSection(sections);
 
@@ -78,6 +94,8 @@ $(document).ready(function () {
         if (deltaY < 0 && section.prevSection.length) {
             performTransition(section.prevSection.index());
         }
+        },
+        
     });
 
     $('[data-scroll-to]').on('click', function (e) {
@@ -90,7 +108,7 @@ $(document).ready(function () {
     $('.wrapper').on('swipe', function (e) {
         var deltaY = e.originalEvent.deltaY,
             section = defineSection(sections);
-    console.log(e.originalEvent.deltaY);
+        console.log(e.originalEvent.deltaY);
         if (deltaY > 0 && section.nextSection.length) {
             performTransition(section.nextSection.index());
             console.log(deltaY);
@@ -105,23 +123,31 @@ $(document).ready(function () {
         e.preventDefault();
         section = defineSection(sections);
         if (e.keyCode == 38 && section.nextSection.length) {
-        performTransition(section.nextSection.index());
+            performTransition(section.nextSection.index());
         }
         else if (e.keyCode == 40 && section.prevSection.length) {
-          performTransition(section.prevSection.index());
+            performTransition(section.prevSection.index());
         }
 
 
 
-});
+    });
 
-var defineSection = function (sections) {
-    var activeSection = sections.filter('.active');
-    return {
-        nextSection: activeSection.next(),
-        prevSection: activeSection.prev(),
+    var defineSection = function (sections) {
+        var activeSection = sections.filter('.active');
+        return {
+            nextSection: activeSection.next(),
+            prevSection: activeSection.prev(),
+        }
+
     }
-
-}
+    if (isMobile) {
+        $(window).swipe({
+            swipe: function (event, direction, distance, duration, fingerCount, fingerData) {
+                scrollToSection(direction);
+            }
+        });
+    }
 }
 )
+
